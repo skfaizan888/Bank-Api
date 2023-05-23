09724115dbd4a0ec2214795495569e2ee59c12c4
Dhanashri Patil <pdhanashri69@gmail.com>
2023-05-23 16:55:22 +0530

    Added Middle Name

--- Dhanashri.js
+++ Dhanashri.js
@@ -1,6 +1,5 @@
-console.log("My Name is Dhanashri Patil");
+console.log("My Name is Dhanashri Somnath Patil");
 
 console.log(2125 ** 3);
 
-
-console.log(25*25*25*25)
+console.log(25 * 25 * 25 * 25);
